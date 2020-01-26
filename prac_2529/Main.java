38226d38dac1401a3fb328ceeed1566c3aedf5c6
Jiwon <googims330@gmail.com>
2020-01-26 20:04:46 +0900

    부등호, 백트레킹을 이용한 DFS탐색

--- prac_2529/Main.java
+++ prac_2529/Main.java
@@ -3,15 +3,56 @@ package prac_2529;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
+	static int k;
+	static String[] compare;
+	static ArrayList<String> result = new ArrayList<>();
+	static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+
+		k = Integer.parseInt(br.readLine());
+		compare = br.readLine().split(" ");
+		visited = new boolean[10];
+
+		dfs(0, "");
+		System.out.println(result.get(result.size() - 1));
+		System.out.println(result.get(0));
 		br.close();
 	}
 
+	public static void dfs(int len, String str) {
+		if (len == k + 1) {
+			if (chk(str) == true) {
+				result.add(str);
+			}
+		}
+		for (int i = 0; i <= 9; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				dfs(len + 1, str + i);
+				visited[i] = false; //백트레킹 : 모든경우의 수 탐색
+			}
+		}
+	}
+
+	public static boolean chk(String str) {
+		for (int i = 1; i < str.length(); i++) {
+			int j = i - 1;
+			if (compare[j].equals(">")) {
+				if ((str.charAt(i - 1) - '0') < (str.charAt(i) - '0')) {
+					return false;
+				}
+			} else if (compare[j].equals("<")) {
+				if ((str.charAt(i - 1) - '0') > (str.charAt(i) - '0')) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
