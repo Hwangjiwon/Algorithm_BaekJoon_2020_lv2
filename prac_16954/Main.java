4df7a8113c04958b7d3a7f0af577ebebf10e5548
Jiwon <googims330@gmail.com>
2020-02-18 00:51:21 +0900

    Update Main.java

--- prac_16954/Main.java
+++ prac_16954/Main.java
@@ -4,13 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class Main {
 	static String[][] map;
 	static boolean[][] visited;
-	static List<Dot> wall = new LinkedList<>();
+	static Queue<Dot> wall;
 	static int[] dx = { 0, 0, -1, 1, 1, -1, 1, -1, 0 };
 	static int[] dy = { -1, 1, 0, 0, -1, -1, 1, 1, 0 };
 	static int result;
@@ -21,6 +20,8 @@ public class Main {
 		map = new String[8][8];
 		visited = new boolean[8][8];
 		String[] tmp;
+
+		wall = new LinkedList<Dot>();
 		for (int i = 0; i < 8; i++) {
 			tmp = br.readLine().split("");
 			for (int j = 0; j < 8; j++) {
@@ -43,19 +44,15 @@ public class Main {
 		Queue<Dot> user = new LinkedList<Dot>();
 		user.add(new Dot(7, 0));
 
-		while (!user.isEmpty()) {
+		while (!user.isEmpty() && !wall.isEmpty()) {
+
 			Dot dot = user.poll();
 			int x = dot.x;
 			int y = dot.y;
 
-			if (x == 7 && y == 0) {
-				result = 1;
-				continue;
-			}
-
 			if (map[y][x].equals("#")) {
 				result = 0;
-				continue;
+				return;
 			}
 
 			for (int i = 0; i < dx.length; i++) {
@@ -64,25 +61,34 @@ public class Main {
 
 				if (nx < 0 || ny < 0 || nx >= 8 || ny >= 8)
 					continue;
-				
-				if (map[ny][nx].equals("#"))
-					continue;
 
 				if (map[ny][nx].equals(".") && !visited[ny][nx]) {
 					visited[ny][nx] = true;
 					user.add(new Dot(ny, nx));
 				}
-
 			}
 
 			// ∫Æ¿Ãµø
-			for (int i = 0; i < wall.size(); i++) {
-				int wx = wall.get(i).x;
-				int wy = wall.get(i).y;
-
+//			
+//			..###.##
+//			##...#.#
+//			..#.#..#
+//			#.#...#.
+//			.#...#.#
+//			.#.#..##
+//			#..#..#.
+//			..#....#
+			int size = wall.size();
+			for (int k = 0; k < size; k++) {
+				Dot pw = wall.poll();
+				int wx = pw.x;
+				int wy = pw.y;
+				
 				map[wy][wx] = ".";
-				if (wy + 1 < 8)
+				if (wy + 1 < 8) {
 					map[wy + 1][wx] = "#";
+					wall.add(new Dot(wy + 1, wx));
+				}
 			}
 
 			System.out.println();
@@ -92,6 +98,10 @@ public class Main {
 				System.out.println();
 			}
 
+			if (wall.isEmpty() && !user.isEmpty()) {
+				result = 1;
+			} else
+				result = 0;
 		}
 
 	}
