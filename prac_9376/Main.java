1151ac9c73633c0631f5493ba990d42467a33ef5
Jiwon <googims330@gmail.com>
2020-02-29 21:47:08 +0900

    yet

---
+++ prac_9376/Main.java
@@ -0,0 +1,16 @@
+package prac_9376;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		br.close();
+	}
+
+}
