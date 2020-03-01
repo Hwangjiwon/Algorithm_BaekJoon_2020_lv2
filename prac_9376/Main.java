086fb32083cc22167a4623ff576fb76f311145e3
Jiwon <googims330@gmail.com>
2020-03-01 23:38:40 +0900

    yet

--- prac_9376/Main.java
+++ prac_9376/Main.java
@@ -5,10 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int h, w;
+	static char[][] map;
+	static boolean[][] visited;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		h = Integer.parseInt(input[0]);
+		w = Integer.parseInt(input[1]);
+
+		map = new char[h][w];
+		visited = new boolean[h][w];
 		
 		br.close();
 	}
