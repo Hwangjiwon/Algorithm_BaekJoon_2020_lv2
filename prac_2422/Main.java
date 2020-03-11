9408c6f9c3b10ac8b6d35a146d1203a6245c6aa3
Jiwon <googims330@gmail.com>
2020-03-12 00:07:52 +0900

    yet

--- prac_2422/Main.java
+++ prac_2422/Main.java
@@ -3,9 +3,20 @@ package prac_2422;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+class Disable {
+	int x, y;
+
+	Disable(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
 
 public class Main {
 	static int N, M;
+	static ArrayList<Disable> list = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -14,6 +25,10 @@ public class Main {
 		N = Integer.parseInt(str[0]);
 		M = Integer.parseInt(str[1]);
 
+		for (int i = 0; i < M; i++) {
+			str = br.readLine().split(" ");
+			list.add(new Disable(Integer.parseInt(str[0]), Integer.parseInt(str[1])));
+		}
 		dfs(0, 1, "");
 
 		br.close();
@@ -22,11 +37,30 @@ public class Main {
 	public static void dfs(int len, int idx, String str) {
 		if (len == 3) {
 			System.out.println(str);
+			if (chk(str)) {
+
+			}
 			return;
 		}
 
 		for (int i = idx; i <= N; i++) {
 			dfs(len + 1, i + 1, str + i + " ");
 		}
+	}
+
+	public static boolean chk(String str) {
+		String[] tmp = str.split(" ");
+		boolean[] visited = new boolean[tmp.length + 1];
+
+		for (int i = 0; i < tmp.length; i++)
+			visited[Integer.parseInt(tmp[i])] = true;
+
+		for (int i = 0; i < M; i++) {
+			System.out.println(list.get(i).x + " " + list.get(i).y);
+			if (visited[list.get(i).x] && visited[list.get(i).y]) {
+				System.out.println("&&&");
+			}
+		}
+		return true;
 	}
 }
