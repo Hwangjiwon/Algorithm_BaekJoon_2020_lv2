1c67c075fd7403baa8f91911ccff038a999e6586
Jiwon <googims330@gmail.com>
2020-03-11 23:31:38 +0900

    yet

--- prac_2422/Main.java
+++ prac_2422/Main.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int N, M;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		
 		
 		br.close();
 	}
