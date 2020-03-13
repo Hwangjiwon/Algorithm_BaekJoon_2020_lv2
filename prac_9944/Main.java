ee8168ae8881e97d250c27194b40497509252cd7
Jiwon <googims330@gmail.com>
2020-03-14 00:01:49 +0900

    yet

---
+++ prac_9944/Main.java
@@ -0,0 +1,20 @@
+package prac_9944;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		while ((str = br.readLine()) != null) {
+			System.out.println("***");
+			System.out.println();
+		}
+		br.close();
+	}
+
+}
