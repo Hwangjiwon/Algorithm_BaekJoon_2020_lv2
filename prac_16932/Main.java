851fcaa08924c3a04ddf49f85689220782b2b910
Jiwon <googims330@gmail.com>
2020-03-15 15:59:56 +0900

    yet

---
+++ prac_16932/Main.java
@@ -0,0 +1,45 @@
+package prac_16932;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N, M;
+	static int[][] map;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+
+		map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			input = br.readLine().split(" ");
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(input[j]);
+			}
+		}
+		
+		print(map);
+
+		br.close();
+	}
+
+	public static void dfs() {
+		
+	}
+
+	
+	public static void print(int[][] map) {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
