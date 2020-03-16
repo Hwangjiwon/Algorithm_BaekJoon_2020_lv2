4908014a3a016fd3ca977c54938084e358d48fb4
Jiwon <googims330@gmail.com>
2020-03-17 00:09:23 +0900

    yet

--- prac_16932/Main.java
+++ prac_16932/Main.java
@@ -7,8 +7,11 @@ import java.io.InputStreamReader;
 public class Main {
 	static int N, M;
 	static int[][] map;
+	static int[][] group;
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
 	static boolean[][] visited;
-	
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,20 +27,28 @@ public class Main {
 				map[i][j] = Integer.parseInt(input[j]);
 			}
 		}
-		
+
 		print(map);
 
 		br.close();
 	}
 
-	public static void dfs() {
-		
+	public static void dfs(int[][] map) {
+		int cnt = 1;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (!visited[i][j] && map[i][j] == 1) {
+					visited[i][j] = true;
+					group[i][j] = cnt;
+				}
+			}
+		}
 	}
 
-	
 	public static void print(int[][] map) {
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
 				System.out.print(map[i][j]);
 			}
 			System.out.println();
